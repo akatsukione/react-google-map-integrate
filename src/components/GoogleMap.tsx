893fb5fd41d6558525e11e3d9fa9a7d2b792e74e
akatsukione <devpand168943@gmail.com>
2025-03-17 11:46:48 +0900

    initial commit for google map api integrate

---
+++ src/components/GoogleMap.tsx
@@ -0,0 +1,26 @@
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+
+const MapComponent = () => {
+  const mapStyles = {
+    height: "400px",
+    width: "100%",
+  };
+
+  const defaultCenter = {
+    lat: 48.8566, // Paris, France latitude
+    lng: 2.3522, // Paris, France longitude
+  };
+
+  return (
+    <LoadScript googleMapsApiKey={import.meta.env.GOOGLE_MAPS_API_KEY}>
+      <GoogleMap
+        mapContainerStyle={mapStyles}
+        zoom={13}
+        center={defaultCenter}
+        mapTypeId="satellite" // This sets the map to satellite view
+      ></GoogleMap>
+    </LoadScript>
+  );
+};
+
+export default MapComponent;
